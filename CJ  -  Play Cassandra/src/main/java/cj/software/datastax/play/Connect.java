4527708821bdcf2ccde360d5bf26ebaccf24b724
cjacobme <cjacobme@aol.com>
2017-08-17 20:37:20 +0200

    display more metadata

--- CJ  -  Play Cassandra/src/main/java/cj/software/datastax/play/Connect.java
+++ CJ  -  Play Cassandra/src/main/java/cj/software/datastax/play/Connect.java
@@ -1,10 +1,17 @@
 package cj.software.datastax.play;
 
+import java.net.InetAddress;
+import java.util.Set;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Configuration;
+import com.datastax.driver.core.Host;
 import com.datastax.driver.core.Metadata;
+import com.datastax.driver.core.ProtocolOptions;
+import com.datastax.driver.core.ProtocolVersion;
 
 public class Connect
 {
@@ -24,6 +31,20 @@ public class Connect
 		{
 			Metadata lMeta = lCluster.getMetadata();
 			logger.info("Connected to cluster \"%s\" meta \"%s\"", lCluster.getClusterName(), lMeta.getClusterName());
+
+			Set<Host> lHosts = lMeta.getAllHosts();
+			for (Host bHost : lHosts)
+			{
+				String lDatacenter = bHost.getDatacenter();
+				String lRack = bHost.getRack();
+				InetAddress lAddress = bHost.getAddress();
+				logger.info("Host: Datacenter = %s, Rack = %s, Address = %s", lDatacenter, lRack, lAddress);
+			}
+
+			Configuration lConfiguration = lCluster.getConfiguration();
+			ProtocolOptions lProtocolOptions = lConfiguration.getProtocolOptions();
+			ProtocolVersion lProtocolVersion = lProtocolOptions.getProtocolVersion();
+			logger.info("Protocol Version: %s", lProtocolVersion);
 		}
 	}
 }
